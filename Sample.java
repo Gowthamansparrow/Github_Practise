13ef5a88521f86ab90e28d033c216ef8f83b5c1d
gowthaman <gowthamansparrow@gmail.com>
2023-10-05 19:02:02 +0530

    Added a java program

---
+++ Sample.java
@@ -0,0 +1,15 @@
+public class Sample
+{
+
+    public static void function_1()
+    {
+        println("Just a sample function");
+    }
+
+
+   
+
+    public static void main(String[] args) {
+        Sample.function_1();
+    }
+}
